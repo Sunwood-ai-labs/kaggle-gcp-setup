33e3ddcf76a7979641cd12e06930f0f3bec4d28f
Maki <sunwood.ai.labs@gmail.com>
2025-02-15 02:37:37 +0900

    ♻️ refactor #4: Cloud Storageバケットのプロジェクト参照を更新
    - プロジェクト参照をdata sourceに変更
    - バケット名とリージョンを設定
    - uniform_bucket_level_accessを有効化

--- bucket.tf
+++ bucket.tf
@@ -2,6 +2,6 @@
 resource "google_storage_bucket" "bucket" {
   name                        = var.bucket_name
   location                    = var.region
-  project                     = google_project.project.project_id
+  project                     = data.google_project.project.project_id
   uniform_bucket_level_access = true
 }
