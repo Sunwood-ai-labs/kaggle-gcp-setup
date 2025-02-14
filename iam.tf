d32961c5716d58688629b5fbd735197999cafb04
Maki <sunwood.ai.labs@gmail.com>
2025-02-15 02:34:13 +0900

    ♻️ refactor #4: IAMリソースのプロジェクト参照を更新
    - プロジェクト参照をdata sourceに変更
    - Storage Admin, Object Creator, Object Viewerの権限を設定

--- iam.tf
+++ iam.tf
@@ -1,18 +1,18 @@
 # IAMロール
 resource "google_project_iam_member" "storage_admin" {
-  project = google_project.project.project_id
+  project = data.google_project.project.project_id
   role    = "roles/storage.admin"
   member  = "serviceAccount:${google_service_account.service_account.email}"
 }
 
 resource "google_project_iam_member" "storage_object_viewer" {
-  project = google_project.project.project_id
+  project = data.google_project.project.project_id
   role    = "roles/storage.objectViewer"
   member  = "serviceAccount:${google_service_account.service_account.email}"
 }
 
 resource "google_project_iam_member" "storage_object_creator" {
-  project = google_project.project.project_id
+  project = data.google_project.project.project_id
   role    = "roles/storage.objectCreator"
   member  = "serviceAccount:${google_service_account.service_account.email}"
 }
