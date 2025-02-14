197047a7c10ddb32f78304084f2b9b0a4e7aea23
Maki <sunwood.ai.labs@gmail.com>
2025-02-15 02:34:43 +0900

    ♻️ refactor #4: サービスアカウントのプロジェクト参照を更新
    - プロジェクト参照をdata sourceに変更
    - Kaggle用のサービスアカウントを設定

--- service_account.tf
+++ service_account.tf
@@ -2,5 +2,5 @@
 resource "google_service_account" "service_account" {
   account_id   = var.service_account_id
   display_name = "Kaggle Service Account"
-  project      = google_project.project.project_id
+  project      = data.google_project.project.project_id
 }
