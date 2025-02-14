ee2003f73ffc1e1517b8d9554b8f0f6ce85c849f
Maki <sunwood.ai.labs@gmail.com>
2025-02-15 02:33:50 +0900

    ♻️ refactor #4: プロジェクトリソースをdata sourceに変更
    - 既存プロジェクトを使用するようにリファクタリング
    - google_projectリソースをdata sourceに変更

--- project.tf
+++ project.tf
@@ -1,7 +1,4 @@
-# Google Cloudプロジェクト
-resource "google_project" "project" {
-  name            = var.project_name
-  project_id      = var.project_id
-  org_id          = var.org_id
-  billing_account = var.billing_account
+# 既存のGoogle Cloudプロジェクトの参照
+data "google_project" "project" {
+  project_id = var.project_id
 }
