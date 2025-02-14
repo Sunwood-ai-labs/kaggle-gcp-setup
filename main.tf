36b39acbf5e7c0a0ba770e90bd24ecc0c43df389
Maki <sunwood.ai.labs@gmail.com>
2025-02-15 01:57:53 +0900

    ✨ refactor: プロジェクトとプロバイダーの情報を main.tf に移動

--- main.tf
+++ main.tf
@@ -19,37 +19,3 @@ resource "google_project" "project" {
   org_id          = var.org_id
   billing_account = var.billing_account
 }
-
-# サービスアカウント
-resource "google_service_account" "service_account" {
-  account_id   = var.service_account_id
-  display_name = "Kaggle Service Account"
-  project      = google_project.project.project_id
-}
-
-# IAMロール
-resource "google_project_iam_member" "storage_admin" {
-  project = google_project.project.project_id
-  role    = "roles/storage.admin"
-  member  = "serviceAccount:${google_service_account.service_account.email}"
-}
-
-resource "google_project_iam_member" "storage_object_viewer" {
-  project = google_project.project.project_id
-  role    = "roles/storage.objectViewer"
-  member  = "serviceAccount:${google_service_account.service_account.email}"
-}
-
-resource "google_project_iam_member" "storage_object_creator" {
-  project = google_project.project.project_id
-  role    = "roles/storage.objectCreator"
-  member  = "serviceAccount:${google_service_account.service_account.email}"
-}
-
-# Cloud Storageバケット
-resource "google_storage_bucket" "bucket" {
-  name                        = var.bucket_name
-  location                    = var.region
-  project                     = google_project.project.project_id
-  uniform_bucket_level_access = true
-}
